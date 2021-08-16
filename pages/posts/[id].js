a70dd5a51de46b243bc80a30ec435edd411b1325
yaoc <yyl134934@163.com>
2021-08-16 17:21:28 +0800

    nextjs blog

---
+++ pages/posts/[id].js
@@ -0,0 +1,30 @@
+import { getAllPostIds,getPostData  } from '../../lib/posts'
+
+export async function getStaticPaths() {
+    const paths = getAllPostIds()
+    return {
+      paths,
+      fallback: false
+    }
+  }
+
+  export async function getStaticProps({ params }) {
+    const postData = await getPostData(params.id)
+    return {
+      props: {
+        postData
+      }
+    }
+  }
+
+export default function Post({postData}) {
+    return <div>      
+        {postData.title}
+    <br />
+    {postData.id}
+    <br />
+    {postData.date}
+    <br/>
+    <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+    </div>
+}
